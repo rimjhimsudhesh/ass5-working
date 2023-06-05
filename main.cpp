3e85b7ac22c7273af59001dfe36db9bd6c5ff55b
rimjhimsudhesh <110277428+rimjhimsudhesh@users.noreply.github.com>
2023-06-04 18:47:04 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,27 +5,51 @@
 #include <iostream>
 #define MAX_VALUE 65536
 using namespace std;
-//#include "menu_options.cpp"
-#include "threaded_binary_tree.cpp"
+#include "threaded_binary_tree.h"
 
-/* Main Contains Menu */
 
 int main() {
-  // menu_options menu_options;
-  ThreadedBinarySearchTree tbst;
+  /*ThreadedBinarySearchTree tbst;
   tbst.insert(1);
   tbst.insert(2);
   tbst.insert(3);
   tbst.insert(4);
   tbst.insert(5);
-  tbst.insert(8);
-  /*  Display tree  */
+  ThreadedBinarySearchTree copy(tbst);
   cout << "\nTree = ";
-  tbst.printTree();
-  tbst.Delete(3);
-  /*  Display tree  */
-  cout << "\n After removal, Tree = ";
-  tbst.printTree();
+  cout << tbst << endl;
+  tbst.remove(2);
+  cout << tbst.search(4)->key << endl;
 
-  return 0;
+  //cout << tbst.contains(4) << endl;
+  //cout << tbst.contains(10) << endl;
+  cout << "\nAfter removal, Tree = ";
+  cout << tbst << endl;
+  
+
+  cout << "Test with input" << endl;
+  ThreadedBinarySearchTree tree(10);
+  tree.remove(5);
+  cout << tree << endl;
+  cout << "Copy!!" << endl;
+  cout << copy << endl;
+
+  return 0; */
+  int input;
+  cout << "Enter number of nodes in tree: ";
+  cin >> input;
+  //cout << input << endl;
+
+  ThreadedBinarySearchTree test(input);
+  ThreadedBinarySearchTree copy(test);
+  int highest = copy.getHighest();
+
+  for(int i = 1; i <= highest; i++) {
+    if(copy.contains(i) && i % 2 == 0) {
+      copy.remove(i);
+
+    }
+  }
+  cout << "Original: " << test << endl;
+  cout << "Copy without evens: " << copy << endl;
 }
